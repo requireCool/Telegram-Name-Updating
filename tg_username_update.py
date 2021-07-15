3311b2b0e797cc35fe1bb43f689f793547dddbd7
requireCool <1019715750@qq.com>
2021-07-15 14:20:44 +0000

    update tg_username_update.py.

--- tg_username_update.py
+++ tg_username_update.py
@@ -38,7 +38,7 @@ logger = logging.getLogger(__name__)
 
 async def change_name_auto():
     # Set time zone to UTC+8
-    ln -sf /usr/share/zoneinfo/Asia/Chongqing /etc/localtime
+    # ln -sf /usr/share/zoneinfo/Asia/Chongqing /etc/localtime
     # https://stackoverflow.com/questions/4788533/python-strftime-gmtime-not-respecting-timezone
 
     print('begin to change name')
